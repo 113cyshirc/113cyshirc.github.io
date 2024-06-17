d8d584d87a284f11766e1ad8914b278e405e3a9a
KnowScratcher <yianlee2008@gmail.com>
2024-06-17 14:36:54 +0800

    更改menu運作程式

--- menu.js
+++ menu.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
     $("#open_menu").click(function () {
-        $(".menu").animate(
-            {
-                width: "toggle",
-            },
-            function () {
-                if ($(".control").length === 0) {
-                    $(".control_o").attr("class", "control");
-                } else {
-                    $(".control").attr("class", "control_o");
-                }
-            }
-        );
+        if ($(".control_o").length == 1) {//收回
+            $(".control_o").animate({ height: "10%" });
+            $(".control_o").removeClass("control_o");
+            $(".menu").animate({ width: "toggle" },function() {
+                $(".control").removeAttr("style");
+            });
+        } else {
+            //伸出
+            $(".menu").animate({ width: "toggle" });
+            $(".control").addClass("control_o");
+            $(".control_o").animate({ height: "100%" });
+        }
     });
     $(".menu_tile").click(function () {
         var this_widget = $(this);
@@ -21,7 +21,7 @@ $(document).ready(function () {
             },
             function () {
                 $(".control_o").attr("class", "control");
-                window.location.replace(this_widget.attr("link"));
+                window.open(this_widget.attr("link"), "_self");
             }
         );
     });
